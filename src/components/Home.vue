0ff990b3e4a07f3ab0d895286a811841bb127794
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-05-15 23:03:32 +0200

    set up vue-router

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  Home<br>
+<br>
+  Todo: Fix webpack config, to load .vue files
+</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import Vuetify from 'vuetify';
 import store from './vuex/store';
+import router from './router';
 
 Vue.use(Vuetify);
 
 new Vue({
   store,
+  router,
   el: '#app',
   render: h => h(App)
 });
