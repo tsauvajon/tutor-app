028d0c2e865a955fe9ccc8a413971b169b8c59c4
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-05-23 21:47:10 +0200

    bind courses with firebase

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,68 +1,116 @@
 <template>
 <div>
   <v-card>
-    <v-card-row>
-      Cours | [Filtrer]
-    </v-card-row>
-    <v-card-row>
-      <v-expansion-panel>
-        <v-expansion-panel-content v-for="cours in allcourses" :key="cours.id">
-          <div slot="header">
-            {{ cours.title }}
-          </div>
-          <v-card class="grey lighten-5">
-            <v-card-title>
-              <div class="row">
-                <v-chip v-for="(chip, i) in cours.chips" :key="i" class="accent white--text" small>
-                  {{ chip }}
-                </v-chip>
-              </div>
-            </v-card-title>
-            <v-card-text>
-              {{ cours.desc }}
-            </v-card-text>
-          </v-card>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-card-row>
+    <v-card-title>
+      Cours
+      <v-spacer />
+      <div>
+        <v-menu bottom left origin="top right">
+          <v-btn icon="icon" slot="activator" class="">
+            <v-icon>filter_list</v-icon>
+          </v-btn>
+          <v-list>
+            <v-list-item>
+              <v-list-tile>
+                <v-list-tile-title>Filtre 1</v-list-tile-title>
+              </v-list-tile>
+            </v-list-item>
+            <v-list-item>
+              <v-list-tile>
+                <v-list-tile-title>Filtre 2</v-list-tile-title>
+              </v-list-tile>
+            </v-list-item>
+            <v-list-item>
+              <v-list-tile>
+                <v-list-tile-title>Filtre 3</v-list-tile-title>
+              </v-list-tile>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </v-card-title>
   </v-card>
+  <v-expansion-panel>
+    <v-expansion-panel-content v-if="course.title" v-for="course in courses" :key="course.createdat">
+      <div slot="header">
+        {{ course.title }}
+      </div>
+      <v-card class="grey lighten-5">
+        <v-card-title>
+          <div class="row">
+            <v-chip v-for="(chip, i) in course.chips" :key="i" class="accent white--text" small>
+              {{ chip }}
+            </v-chip>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          {{ course.description }}
+          <br><br> Création : {{ new Date(course.createdAt).toString() }}
+        </v-card-text>
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+  <!-- </v-card-row>
+  </v-card> -->
+  <!-- <pre v-for="key in Object.keys(courses)">
+    {{ courses[key] }}
+  </pre> -->
+  <v-btn primary floating large light class="fab">
+    <v-icon class="white--text">add</v-icon>
+  </v-btn>
 </div>
 </template>
 
 <script>
+import {
+  mapGetters
+} from 'vuex';
+
 export default {
   name: 'courses',
-  data: () => ({
-    allcourses: [{
-        id: 0,
-        title: 'Initiation à React',
-        desc: 'un cours sur React',
-        chips: [
-          'javascript',
-          'développement',
-          'front-end',
-        ],
-      },
-      {
-        id: 1,
-        title: 'Démarrer avec Vue',
-        desc: 'un cours sur Vue',
-        chips: [
-          'javascript',
-          'développement',
-          'front-end',
-        ],
-      },
-      {
-        id: 2,
-        title: 'Réviser l\'IPI',
-        desc: 'révisions de l\'IPI',
-        chips: [
-          'réseau',
-          'révisions',
-        ],
-      },
-    ],
-  }),
+
+  created() {
+    const database = this.$store.getters.fbApp.database();
+    const coursesRef = database.ref('courses');
+    this.$store.dispatch('setCoursesRef', coursesRef);
+  },
+
+  computed: mapGetters(['courses']),
+
+  // computed()
+
+  // data: () => ({
+  //   allcourses: [{
+  //       id: 0,
+  //       title: 'Initiation à React',
+  //       desc: 'un cours sur React',
+  //       chips: [
+  //         'javascript',
+  //         'développement',
+  //         'front-end',
+  //       ],
+  //     },
+  //     {
+  //       id: 1,
+  //       title: 'Démarrer avec Vue',
+  //       desc: 'un cours sur Vue',
+  //       chips: [
+  //         'javascript',
+  //         'développement',
+  //         'front-end',
+  //       ],
+  //     },
+  //     {
+  //       id: 2,
+  //       title: 'Réviser l\'IPI',
+  //       desc: 'révisions de l\'IPI',
+  //       chips: [
+  //         'réseau',
+  //         'révisions',
+  //       ],
+  //     },
+  //   ],
+  // }),
+
 };
 </script>
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { firebaseMutations, firebaseAction } from 'vuexfire';
 
 Vue.use(Vuex);
 
@@ -9,30 +10,41 @@ const state = {
   user: null,
   fbApp: null,
   fbUiApp: null,
+  courses: null,
 };
 
 const mutations = {
   SET_USER(state, user) {
     state.user = user;
   },
+
   SET_FB_APP(state, fbApp) {
     state.fbApp = fbApp;
   },
+
   SET_FB_UI_APP(state, fbUiApp) {
     state.fbUiApp = fbUiApp;
   },
+
+  ...firebaseMutations,
 };
 
 const actions = {
   setUser: ({ commit }, user) => commit('SET_USER', user),
   setFbApp: ({ commit }, fbApp) => commit('SET_FB_APP', fbApp),
   setFbUiApp: ({ commit }, fbUiApp) => commit('SET_FB_UI_APP', fbUiApp),
+  setCoursesRef: firebaseAction(({
+    bindFirebaseRef,
+  }, ref) => {
+    bindFirebaseRef('courses', ref);
+  }),
 };
 
 const getters = {
   user: state => state.user,
   fbApp: state => state.fbApp,
   fbUiApp: state => state.fbUiApp,
+  courses: state => state.courses,
 };
 
 export default new Vuex.Store({
