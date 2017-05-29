10b6ad745e59caf09d0a9e3cc90ac6fb1493a0ca
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-05-29 16:10:19 +0200

    fixing script

--- travis.sh
+++ travis.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [[ $TRAVIS_PULL_REQUEST = "false" ]]
-  - yarn global add firebase-tools
-  - cd functions
-  - yarn
-  - cd ..
-  - yarn lint
-  - yarn test
-  - yarn build
-  - cp public/* dist
-  - firebase deploy --token ${FIREBASE_TOKEN}
+if [ $TRAVIS_PULL_REQUEST = "false" ];
+  yarn global add firebase-tools;
+  cd functions;
+  yarn;
+  cd ..;
+  yarn lint;
+  yarn test;
+  yarn build;
+  cp public/* dist;
+  firebase deploy --token ${FIREBASE_TOKEN};
 else
-  - yarn lint
-  - yarn test
-  - yarn build
+  yarn lint;
+  yarn test;
+  yarn build;
 fi
