323644cd6501211eb72dc34a14c79b94128ae2ce
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-05-31 09:10:51 +0200

    disable noy implemented options

--- src/components/Admin/Params.vue
+++ src/components/Admin/Params.vue
@@ -4,8 +4,8 @@
     <v-card-title>Paramètres</v-card-title>
     <v-card-text>
       <v-text-field label="Nom de l'établissement" value="EPSI Montpellier" />
-      <v-switch v-model="openInscription" label="Inscription ouverte à tous" />
-      <v-switch v-model="roomManagement" label="Gestion des salles dans l'appli" />
+      <v-switch disabled v-model="openInscription" label="Inscription ouverte à tous" />
+      <v-switch disabled v-model="roomManagement" label="Gestion des salles dans l'appli" />
     </v-card-text>
   </v-card>
 </div>
